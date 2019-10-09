c29faf1247fbde49d5e653d11e217e18a979cb4b
Felipe Flores <felipe.flores@ing.uchile.cl>
2019-10-09 16:09:08 -0300

    Agregar formulario de registro
    Se agrega formulario de registro, página base para el formulario, y página que indica éxito del registro de usuario.

--- ProyectoDjango/tarea4/forms.py
+++ ProyectoDjango/tarea4/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 
+
 class IniciarSesionForm(forms.Form):
     user = forms.CharField()
     password = forms.CharField()
 
+
 class nombrexdForm(forms.Form):
     nombrexd = forms.CharField(label='nombrexd', max_length=100)
+
+
+class RegisterForm(forms.Form):
+    name = forms.CharField(max_length=140)
+    lastname = forms.CharField(max_length=140)
+    password = forms.CharField(max_length=140)
+    email = forms.CharField(max_length=140)
--- ProyectoDjango/tarea4/views.py
+++ ProyectoDjango/tarea4/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .forms import IniciarSesionForm
+from .forms import RegisterForm
+from .models import *
+
 
 # Create your views here.
 def landingpage(request):
     return render(request, 'LandingPage.html')
 
+
 def perfil(request):
     return render(request, 'UserProfile.html')
+
 
 def login(request):
     if request.method == 'POST':
@@ -14,10 +19,27 @@ def login(request):
         if login_form.is_valid():
             user = login_form.cleaned_data['user']
             password = login_form.cleaned_data['password']
-
             return render(request, 'LogIn.html', {'nombrexd': user, 'resultados': password})
 
     else:
         login_form = IniciarSesionForm()
         print("jajaj ayuda")
         return render(request, 'LogIn.html', {'resultados': "what"})
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            name = register_form.cleaned_data['name']
+            lastname = register_form.cleaned_data['lastname']
+            email = register_form.cleaned_data['email']
+            password = register_form.cleaned_data['password']
+            emlen = len(email)
+            user = User.objects.create_user(name, email, password)
+            user.save()
+            email = email[0:3] + "******" + email[emlen-5:emlen]
+
+            return render(request, 'SuccesReg.html', {'nombre': (name + " " +  lastname), 'mail': email})
+    else:
+        return render(request, 'Register.html')
