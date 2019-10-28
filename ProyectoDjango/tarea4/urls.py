059531662ce27129e8e591f8af66281be80ebb23
Tomas Torres <tomas.torresbardavid@gmail.com>
2019-10-28 13:52:03 -0300

    cambio de contraseña

--- ProyectoDjango/tarea4/urls.py
+++ ProyectoDjango/tarea4/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from tarea4 import views
 
 urlpatterns = [
     path('', login),
@@ -9,5 +11,7 @@ urlpatterns = [
     path('inicio/', landingpage, name='landingpage'),
     path('perfil/',perfil,name='perfil'),
     path('logout/',logout,name='logout'),
-    path('perfil/cambioImagen/',cambioImagen, name='cambioImagen')
+    path('perfil/cambioImagen/',cambioImagen, name='cambioImagen'),
+    url('perfil/cambioContraseña/', views.cambioContraseña, name='cambioContraseña'),
+    #path('perfil/cambioContraseña/',cambioContraseña, name='cambioContraseña')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ProyectoDjango/tarea4/views.py
+++ ProyectoDjango/tarea4/views.py
@@ -6,9 +6,12 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as do_login
 from django.contrib.auth import logout as do_logout
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from .forms import CambioDeImagenForm
-
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext as _
 
 # Create your views here.
 def landingpage(request):
@@ -62,3 +65,19 @@ def cambioImagen(request):
     else:
         form = CambioDeImagenForm()
     return render(request, 'CambioDeImagen.html', {'form': form})
+
+def cambioContraseña(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, _('¡Se ha actualizado la contraseña correctamente!'))
+            return HttpResponseRedirect('/perfil/')
+        else:
+            messages.error(request, _('Porfavor corrregir el error siguiente.'))
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'cambioDeContraseña.html', {
+        'form': form
+    })
